f9142a03085b14cc52b40e574933dabc0f7f34dd
Clare <clarepan0@gmail.com>
2026-01-31 15:12:48 -0800

    add enemy behaviours

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,23 +1,57 @@
 extends CharacterBody2D
 
+const ENEMY_SPEED = 100
+
 var direction = 1
-var speed = 1000.0
-var distance = 10
+var speed = 0
 var start_position = position.x
 
+var current_mask = GameManager.GAME_STATE.HAPPY
+
+func _ready() -> void:
+	GameManager.on_game_state_changed.connect(handle_emotion_state_changed)
+	
+func handle_emotion_state_changed(state: GameManager.GAME_STATE) -> void:
+	print(state)
+	## do stuff
+	match state:
+		GameManager.GAME_STATE.HAPPY:
+			current_mask = GameManager.GAME_STATE.HAPPY
+		GameManager.GAME_STATE.ANGRY:
+			current_mask = GameManager.GAME_STATE.ANGRY
+		GameManager.GAME_STATE.SAD:
+			current_mask = GameManager.GAME_STATE.SAD
 
 func _physics_process(delta: float) -> void:
-	#start_position = position.x
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	velocity.x = speed * direction * delta
+	match current_mask:
+		GameManager.GAME_STATE.HAPPY:
+			var player = get_node("../Player")
+			var player_pos_x = player.position.x
+			if (position.x <= player_pos_x):
+				direction = 1
+			else:
+				direction = -1
+
+			speed = ENEMY_SPEED
+			velocity.x = speed * direction
+			move_and_slide()
+		GameManager.GAME_STATE.ANGRY:
+			var player = get_node("../Player")
+			var player_pos_x = player.position.x
+			
+			if (position.x <= player_pos_x):
+				direction = -1
+			else:
+				direction = 1
+			
+			speed = ENEMY_SPEED
+			velocity.x = speed * direction
+			move_and_slide()
+		GameManager.GAME_STATE.SAD:
+			speed = 0
+			
 	
-	if (position.x <= start_position - distance):
-		direction = 1
-	if (position.x >= start_position + distance):
-		direction = -1
-	
-	move_and_slide()
