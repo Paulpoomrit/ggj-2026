bdfc9f5e55104e0e57098d83914e2792390c75ef
Paulpoomrit <paulpoomrit@gmail.com>
2026-02-01 13:50:38 -0800

    Updated UI

--- game-manager/SFXManager.gd
+++ game-manager/SFXManager.gd
@@ -1,6 +1,15 @@
 extends Node
 
 
+const ClickSounds: Array[AudioStream] = [
+	preload("uid://bwgxegbup4qaf"),
+	preload("uid://d1v53u2aeeaun"),
+	preload("uid://br7mf7e6peods"),
+	preload("uid://cngssuag03n2i"),
+	preload("uid://crlq6053wxb5w"),
+]
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +18,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func play_click_sfx(player: AudioStreamPlayer) -> void:
+	player.stop()
+	player.stream = ClickSounds.pick_random()
+	player.play()
