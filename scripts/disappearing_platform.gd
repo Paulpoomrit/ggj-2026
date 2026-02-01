dd04294aa6138f8bacd025a24e45980cdbe224bf
Paulpoomrit <paulpoomrit@gmail.com>
2026-02-01 10:45:02 -0800

    Added platform logic

---
+++ scripts/disappearing_platform.gd
@@ -0,0 +1,18 @@
+extends TileMapLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.visible = false
+	self.collision = false
+	GameManager.on_pressure_plate_state_changed.connect(on_pressure_plate_state_changed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func on_pressure_plate_state_changed(should_open: bool):
+	self.visible = should_open
+	self.collision_enabled = true
