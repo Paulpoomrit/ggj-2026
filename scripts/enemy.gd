ac2b6651421562dd0e77e3090c04801bd132347f
Clare <clarepan0@gmail.com>
2026-01-31 16:20:21 -0800

    add switch case default

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -7,6 +7,8 @@ var direction = 1
 var speed = 0
 var start_position = position.x
 
+var distance = 50
+
 var current_mask = GameManager.GAME_STATE.HAPPY
 
 func _ready() -> void:
@@ -22,10 +24,12 @@ func handle_emotion_state_changed(state: GameManager.GAME_STATE) -> void:
 			current_mask = GameManager.GAME_STATE.ANGRY
 		GameManager.GAME_STATE.SAD:
 			current_mask = GameManager.GAME_STATE.SAD
+		_:
+			current_mask = GameManager.GAME_STATE.HAPPY
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add the gravity if ground enemy.
 	if not is_on_floor() && motion_mode == MOTION_MODE_GROUNDED:
 		velocity += get_gravity() * delta
 	
@@ -33,7 +37,6 @@ func _physics_process(delta: float) -> void:
 		GameManager.GAME_STATE.HAPPY:
 			var player = get_node("../Player")
 			var player_radius = player.get_node("CollisionShape2D").shape.radius
-			
 			var player_pos_x = player.position.x + player_radius
 
 			if (position.x <= player_pos_x):
@@ -60,5 +63,12 @@ func _physics_process(delta: float) -> void:
 			move_and_slide()
 		GameManager.GAME_STATE.SAD:
 			speed = 0
+		_:
+			if (position.x <= start_position - distance):
+				direction = 1
+			if (position.x >= start_position + distance):
+				direction = -1
+			
+			
 			
 	
