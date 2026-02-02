bbde8597fbbf0b1185d1e19236e54b36066696b6
PigletP <emily.r.taylor2002@gmail.com>
2026-02-01 17:04:03 -0800

    doors work and levels cycle

--- Gate.gd
+++ Gate.gd
@@ -1,3 +1,4 @@
+class_name Gate
 extends RigidBody2D
 var timer : Timer
 var should_move = false
@@ -5,43 +6,17 @@ var direction = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	timer = $Timer
-	GameManager.on_pressure_plate_state_changed.connect(on_pressure_plate_changed)
+	pass
+	## GameManager.on_pressure_plate_state_changed.connect(on_pressure_plate_changed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_0):
-		should_move = true
-		direction = true
-		timer.start()
-	if Input.is_key_pressed(KEY_1):
-		should_move = true
-		direction = false
-		timer.start()
-		
-	_move()
-
-
-func _on_timer_timeout() -> void:
-	should_move = false
-	
-func _move():
-	if should_move:
-		if direction:
-			move_local_y(3)
-			$CollisionShape2D.disabled = true
-		else:
-			move_local_y(-3)
-			$CollisionShape2D.disabled = false
-			
+	pass		
 
 func on_pressure_plate_changed(is_enable: bool, target_platoform: Node) -> void:
+	print("enable: ", is_enable)
+	print("target: ", target_platoform)
+
 	if is_enable:
-		should_move = true
-		direction = true
-		timer.start()
-	else:
-		should_move = true
-		direction = false
-		timer.start()
-	_move()
+		if target_platoform:
+			target_platoform.queue_free()
