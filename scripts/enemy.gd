cc8c9272cd17e630e3b1df28b1295de4e8711c01
PigletP <emily.r.taylor2002@gmail.com>
2026-02-03 14:50:00 -0800

    combined animations with fixed version

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -49,7 +49,7 @@ func _physics_process(delta: float) -> void:
 			if player == null:
 				return
 				
-			var player_size_x = player.get_node("CollisionShape2D").shape.size.x
+			var player_size_x = player.get_node("CollisionShape2D").shape.radius
 			var player_pos_x = player.position.x + player_size_x
 
 			if (position.x <= player_pos_x):
@@ -64,7 +64,7 @@ func _physics_process(delta: float) -> void:
 			if player == null:
 				return
 			
-			var player_size_x = player.get_node("CollisionShape2D").shape.size.x
+			var player_size_x = player.get_node("CollisionShape2D").shape.radius
 			
 			var player_pos_x = player.position.x + player_size_x
 			
