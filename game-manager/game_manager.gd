ec136fc3f4a59f6550244e8e737221bf5646c759
Paulpoomrit <paulpoomrit@gmail.com>
2026-02-03 13:43:28 -0800

    Added level switching logic

--- game-manager/game_manager.gd
+++ game-manager/game_manager.gd
@@ -7,11 +7,17 @@ enum LEVELS {LEVEL_ONE, LEVEL_TWO, LEVEL_THREE}
 
 signal on_game_state_changed(state: GAME_STATE)
 signal on_pressure_plate_state_changed(is_enable: bool, target_platoform: Node)
+signal on_switch_to_level(level: LEVELS)
 
+const level_ref: Dictionary[LEVELS, PackedScene] = {
+	LEVELS.LEVEL_ONE: preload("uid://brhxug8pmagid"),
+	LEVELS.LEVEL_TWO: preload("uid://b1pcykdp361ti"),
+	LEVELS.LEVEL_THREE: preload("uid://bc0iahe7g074w")
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	on_switch_to_level.connect(switch_to_level)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,4 +26,4 @@ func _process(delta: float) -> void:
 
 
 func switch_to_level(level: LEVELS) -> void:
-	pass
+	get_tree().change_scene_to_packed(level_ref[level])
--- scripts/exit.gd
+++ scripts/exit.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var level : Resource
+@export var level : GameManager.LEVELS
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,4 +14,7 @@ func _process(delta: float) -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.get_parent().name == "Player":
-		get_tree().change_scene_to_packed(level)
+		# let the game manager handle level switching logic
+		# so that we don't need a direct ref to the level
+		# and just pass in an enum
+		GameManager.on_switch_to_level.emit(level)
