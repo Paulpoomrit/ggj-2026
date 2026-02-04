550b400b8c92558931aba9c926d6b399467762db
Rankirat Singh <rankiratsingh00@gmail.com>
2026-02-03 19:26:19 -0800

    Fixed Level Refs

--- game-manager/game_manager.gd
+++ game-manager/game_manager.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 enum GAME_STATE {HAPPY, ANGRY, SAD}
 enum LEVELS {LEVEL_ONE, LEVEL_TWO, LEVEL_THREE}
 
@@ -10,9 +9,9 @@ signal on_pressure_plate_state_changed(is_enable: bool, target_platoform: Node)
 signal on_switch_to_level(level: LEVELS)
 
 const level_ref: Dictionary[LEVELS, PackedScene] = {
-	LEVELS.LEVEL_ONE: preload("uid://brhxug8pmagid"),
-	LEVELS.LEVEL_TWO: preload("uid://b1pcykdp361ti"),
-	LEVELS.LEVEL_THREE: preload("uid://bc0iahe7g074w")
+	LEVELS.LEVEL_ONE: preload("res://levels/level_one.tscn"),
+	LEVELS.LEVEL_TWO: preload("res://levels/level_two.tscn"),
+	LEVELS.LEVEL_THREE: preload("res://levels/level_three.tscn")
 }
 
 # Called when the node enters the scene tree for the first time.
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -52,7 +52,7 @@ func _physics_process(delta: float) -> void:
 			var player_size_x = player.get_node("CollisionShape2D").shape.radius
 			var player_pos_x = player.position.x + player_size_x
 
-			if (position.x  < player_pos_x):
+			if (position.x + 100 < player_pos_x):
 				direction = 1
 			else:
 				direction = -1
